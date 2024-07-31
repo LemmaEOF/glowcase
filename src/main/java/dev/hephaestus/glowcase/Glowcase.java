e24c0afff29547a53fd0d3e5a0721d5dfe152b63
TheEpicBlock <git.teb@theepicblock.nl>
2024-07-31 20:18:54 +0200

    Remove mailbox
    closes #34

--- src/main/java/dev/hephaestus/glowcase/Glowcase.java
+++ src/main/java/dev/hephaestus/glowcase/Glowcase.java
@@ -2,27 +2,19 @@ package dev.hephaestus.glowcase;
 
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.context.CommandContext;
 import dev.hephaestus.glowcase.block.HyperlinkBlock;
 import dev.hephaestus.glowcase.block.ItemDisplayBlock;
-import dev.hephaestus.glowcase.block.MailboxBlock;
 import dev.hephaestus.glowcase.block.TextBlock;
 import dev.hephaestus.glowcase.block.entity.HyperlinkBlockEntity;
 import dev.hephaestus.glowcase.block.entity.ItemDisplayBlockEntity;
-import dev.hephaestus.glowcase.block.entity.MailboxBlockEntity;
 import dev.hephaestus.glowcase.block.entity.TextBlockEntity;
 import dev.hephaestus.glowcase.compat.PolydexCompatibility;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.command.argument.BlockPosArgumentType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -32,11 +24,8 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
 
 public class Glowcase implements ModInitializer {
 	public static final String MODID = "glowcase";
@@ -53,10 +42,6 @@ public class Glowcase implements ModInitializer {
 	public static final Supplier<BlockItem> ITEM_DISPLAY_BLOCK_ITEM = registerItem("item_display_block", () -> new BlockItem(ITEM_DISPLAY_BLOCK.get(), new Item.Settings()));
 	public static final Supplier<BlockEntityType<ItemDisplayBlockEntity>> ITEM_DISPLAY_BLOCK_ENTITY = registerBlockEntity("item_display_block", () -> BlockEntityType.Builder.create(ItemDisplayBlockEntity::new, ITEM_DISPLAY_BLOCK.get()).build(null));
 
-	public static final Supplier<MailboxBlock> MAILBOX_BLOCK = registerBlock("mailbox", MailboxBlock::new);
-	public static final Supplier<BlockItem> MAILBOX_ITEM = registerItem("mailbox", () -> new BlockItem(MAILBOX_BLOCK.get(), new Item.Settings()));
-	public static final Supplier<BlockEntityType<MailboxBlockEntity>> MAILBOX_BLOCK_ENTITY = registerBlockEntity("mailbox", () -> BlockEntityType.Builder.create(MailboxBlockEntity::new, MAILBOX_BLOCK.get()).build(null));
-
 	public static final Supplier<TextBlock> TEXT_BLOCK = registerBlock("text_block", TextBlock::new);
 	public static final Supplier<BlockItem> TEXT_BLOCK_ITEM = registerItem("text_block", () -> new BlockItem(TEXT_BLOCK.get(), new Item.Settings()));
 	public static final Supplier<BlockEntityType<TextBlockEntity>> TEXT_BLOCK_ENTITY = registerBlockEntity("text_block", () -> BlockEntityType.Builder.create(TextBlockEntity::new, TEXT_BLOCK.get()).build(null));
@@ -67,7 +52,6 @@ public class Glowcase implements ModInitializer {
 		.entries((displayContext, entries) -> {
 			entries.add(HYPERLINK_BLOCK_ITEM.get());
 			entries.add(ITEM_DISPLAY_BLOCK_ITEM.get());
-			entries.add(MAILBOX_ITEM.get());
 			entries.add(TEXT_BLOCK_ITEM.get());
 		})
 		.build()
@@ -97,36 +81,8 @@ public class Glowcase implements ModInitializer {
 	public void onInitialize() {
 		GlowcaseNetworking.init();
 
-		CommandRegistrationCallback.EVENT.register((dispatcher, access, environment) -> {
-			dispatcher.register(
-				LiteralArgumentBuilder.<ServerCommandSource>literal("mail")
-					.then(CommandManager.argument("pos", new BlockPosArgumentType())
-						.then(CommandManager.argument("message", StringArgumentType.greedyString()).executes(this::sendMessage)))
-			);
-		});
-
 		if (FabricLoader.getInstance().isModLoaded("polydex2")) {
 			PolydexCompatibility.onInitialize();
-		}
-	}
-
-	private int sendMessage(CommandContext<ServerCommandSource> ctx) {
-		BlockPos pos = BlockPosArgumentType.getBlockPos(ctx, "pos");
-		String message = ctx.getArgument("message", String.class);
-		PlayerEntity sender = ctx.getSource().getPlayer();
-
-		if (sender != null) {
-			if (sender.getWorld().getBlockEntity(pos) instanceof MailboxBlockEntity mailbox) {
-				mailbox.addMessage(new MailboxBlockEntity.Message(sender.getUuid(), sender.getNameForScoreboard(), message));
-				ctx.getSource().sendFeedback(() -> Text.translatable("command.glowcase.message_sent"), false);
-				return 0;
-			} else {
-				ctx.getSource().sendError(Text.translatable("command.glowcase.failed.no_mailbox"));
-				return 100;
-			}
-		} else {
-			ctx.getSource().sendError(Text.translatable("command.glowcase.failed.no_world"));
-			return 100;
 		}
 	}
 }
--- src/main/java/dev/hephaestus/glowcase/client/GlowcaseClient.java
+++ src/main/java/dev/hephaestus/glowcase/client/GlowcaseClient.java
@@ -1,25 +1,14 @@
 package dev.hephaestus.glowcase.client;
 
 import dev.hephaestus.glowcase.Glowcase;
-import dev.hephaestus.glowcase.block.entity.MailboxBlockEntity;
 import dev.hephaestus.glowcase.client.render.block.entity.BakedBlockEntityRenderer;
 import dev.hephaestus.glowcase.client.render.block.entity.HyperlinkBlockEntityRenderer;
 import dev.hephaestus.glowcase.client.render.block.entity.ItemDisplayBlockEntityRenderer;
 import dev.hephaestus.glowcase.client.render.block.entity.TextBlockEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.InvalidateRenderStateCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
-import net.minecraft.client.util.Window;
-import net.minecraft.text.OrderedText;
-import net.minecraft.text.StringVisitable;
-import net.minecraft.text.Text;
-import net.minecraft.util.hit.BlockHitResult;
-
-import java.util.List;
 
 public class GlowcaseClient implements ClientModInitializer {
 	@Override
@@ -32,48 +21,5 @@ public class GlowcaseClient implements ClientModInitializer {
 
 		WorldRenderEvents.AFTER_TRANSLUCENT.register(BakedBlockEntityRenderer.Manager::render);
 		InvalidateRenderStateCallback.EVENT.register(BakedBlockEntityRenderer.Manager::reset);
-
-		HudRenderCallback.EVENT.register((context, tickDelta) -> {
-			MinecraftClient client = MinecraftClient.getInstance();
-
-			if (client.world != null && client.crosshairTarget instanceof BlockHitResult hitResult && client.world.getBlockEntity(hitResult.getBlockPos()) instanceof MailboxBlockEntity mailbox && mailbox.messageCount() > 0 && mailbox.owner().equals(client.getSession().getUuidOrNull())) {
-				Window window = client.getWindow();
-				TextRenderer textRenderer = client.textRenderer;
-				MailboxBlockEntity.Message message = mailbox.getMessage();
-				List<OrderedText> lines = textRenderer.wrapLines(StringVisitable.plain(message.message()), window.getWidth() / 2);
-				Text reminder2 = Text.translatable("glowcase.mailbox.reminder2");
-
-				int padding = 3;
-
-				int lineHeight = (textRenderer.fontHeight + 3);
-
-				int contentWidth = Math.max(window.getScaledWidth() / 2, textRenderer.getWidth(reminder2));
-				int totalWidth = contentWidth + padding * 2;
-				int totalHeight = (lines.size() + 6) * lineHeight;
-
-				int startX = window.getScaledWidth() / 2 - totalWidth / 2;
-				int startY = window.getScaledHeight() / 2 - totalHeight / 2;
-
-				context.fill(startX, startY, startX + totalWidth, startY + totalHeight, 0x80000000);
-
-				int y = startY + lineHeight * 2;
-
-				for (OrderedText line : lines) {
-					context.drawText(client.textRenderer, line, startX + 3, y, -1, false);
-					y += lineHeight;
-				}
-
-				context.drawText(client.textRenderer, Text.translatable("glowcase.mailbox.sender", message.senderName()), startX + 3, startY + 3, -1, false);
-
-				Text messageCount = Text.literal("1/" + mailbox.messageCount());
-				context.drawText(client.textRenderer, messageCount, startX + totalWidth - 3 - textRenderer.getWidth(messageCount), y + lineHeight, -1, false);
-
-				Text reminder1 = Text.translatable("glowcase.mailbox.reminder1");
-				context.drawText(client.textRenderer, reminder1, startX + totalWidth - 3 - textRenderer.getWidth(reminder1), y + lineHeight * 2, 0xFFAAAAAA, false);
-
-				context.drawText(client.textRenderer, reminder2, startX + totalWidth - 3 - textRenderer.getWidth(reminder2), y + lineHeight * 3, 0xFFAAAAAA, false);
-
-			}
-		});
 	}
 }
